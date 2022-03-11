2778d2665cbfd6f6ac332a25eec9fb1522604d91
alfredo <alide_8@hotmail.com>
2022-03-11 09:40:00 -0600

    configuraciones

---
+++ generaImagen.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+mvn spring-boot:build-image
--- src/test/java/mx/post/PostApplicationTests.java
+++ src/test/java/mx/post/PostApplicationTests.java
@@ -1,4 +1,4 @@
-package mx.post.post;
+package mx.post;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
